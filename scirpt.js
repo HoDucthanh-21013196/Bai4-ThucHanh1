445312b11ef6d08999261b6a518234509b25a5a1
@HoDucthanh-21013196 <21013196@st.phenikaa-uni.edu.vn>
2023-02-27 21:09:07 +0700

    Bai4-ThucHanh1

---
+++ scirpt.js
@@ -0,0 +1,10 @@
+var num1 = 5;
+var num2 = 7;
+console.log("original " + num1);
+console.log("original " + num2);
+var temp = num1;
+num1 = num2;
+num2= temp;
+
+console.log("swap" + num1);
+console.log("swap" + num2);
